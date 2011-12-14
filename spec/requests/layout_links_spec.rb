bd5fec7a69d70073e9ef6ba06646c5bf821a8417
Kestrel Blackmore <kestrel@blackmorewebsites.com.au>
2011-12-14 23:54:54 +1030

    added user controller

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -12,13 +12,18 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Contact")
   end
   
-  it "should have a About page at '/'" do
+  it "should have a About page at '/about'" do
     get '/about'
     response.should have_selector('title', :content => "About")
   end
   
-  it "should have a Help page at '/'" do
+  it "should have a Help page at '/help'" do
     get '/help'
     response.should have_selector('title', :content => "Help")
+  end
+  
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
   end
 end
