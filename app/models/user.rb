89c9ea498e63528757ee44711a4228a5d6b5e929
Kestrel Blackmore <kestrel@blackmorewebsites.com.au>
2011-12-21 01:35:31 +1030

    added user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
   
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
+  
+  has_many :microposts, :dependent => :destroy
     
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -28,6 +30,11 @@ class User < ActiveRecord::Base
                        :confirmation => true,
                        :length => { :within => 6..40 }
      
+  def feed
+    # This is preliminary. See Chapter 12 for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+  
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
   end   
